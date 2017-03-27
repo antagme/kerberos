8112eed4efc83d2402f9a39bc466a2ac993f919f
Pedro Romero Aguado <ff7maniac@gmail.com>
2017-03-27 23:30:15 +0200

    Update kdb5_init.sh

--- scripts/kdb5_init.sh
+++ scripts/kdb5_init.sh
@@ -2,5 +2,4 @@
 KERB_MASTER_KEY="admin"
 REALM="EDT.ORG"
 /usr/sbin/kdb5_util -P $KERB_MASTER_KEY -r $REALM create -s
-cp -f /opt/docker/kdc.conf /var/kerberos/krb5kdc/
-cp -f /opt/docker/kadm5.acl /var/kerberos/krb5kdc/
+
