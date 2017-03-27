83cee44c12b182833c16f7d89f8a5f98433c5cf3
Pedro Romero Aguado <ff7maniac@gmail.com>
2017-03-28 00:39:01 +0200

    Update local_admin.sh

--- scripts/local_admin.sh
+++ scripts/local_admin.sh
@@ -1,5 +1,8 @@
  #! /bin/bash
- # Script per crear el admin principal en local 
+ # Script per crear el admin principal en local i mes princs per ldap
  KERB_ADMIN_PASS=admin
  KERB_ADMIN_USER=admin
  kadmin.local -q "addprinc -pw $KERB_ADMIN_PASS $KERB_ADMIN_USER/admin"
+ kadmin.local -q "addprinc -pw kldap ldap/ldap.edt.org"
+ kadmin.local -q "addprinc -pw khost host/ldap.edt.org"
+ 
