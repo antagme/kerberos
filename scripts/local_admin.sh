56aad51db46821a25245dac133ac06db2f9b6fad
Pedro Romero Aguado <ff7maniac@gmail.com>
2017-03-28 08:23:57 +0200

    Update local_admin.sh

--- scripts/local_admin.sh
+++ scripts/local_admin.sh
@@ -3,6 +3,8 @@
  KERB_ADMIN_PASS=admin
  KERB_ADMIN_USER=admin
  kadmin.local -q "addprinc -pw $KERB_ADMIN_PASS $KERB_ADMIN_USER/admin"
+ kadmin.local -q "addprinc -pw kldap ldap/kserver.edt.org"
+ kadmin.local -q "addprinc -pw khost host/kserver.edt.org"
  kadmin.local -q "addprinc -pw kldap ldap/ldap.edt.org"
  kadmin.local -q "addprinc -pw khost host/ldap.edt.org"
- 
+ kadmin.local -q "addprinc -pw kuser01 user01"
